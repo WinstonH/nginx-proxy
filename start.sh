f77c62e1de66786ccd3659afa5bfd76348b911ec
WinstonH <bjdhjj@126.com>
2017-06-02 03:34:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 echo "0.0.0.0 80 $host 80" >> /etc/rinetd.conf
 
-echo "Loading /etc/rinetd.conf"
-ps -fe|grep rinetd |grep -v grep
-if [ $? -ne 0 ]
-then
-echo "start rinetd ..."
-/usr/sbin/rinetd -c /etc/rinetd.conf
-else
-echo "Restart rinetd"
-SID=`pgrep rinetd`
-kill -9 $SID
-unset SID
-/usr/sbin/rinetd -c /etc/rinetd.conf
-fi
